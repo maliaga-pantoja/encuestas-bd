8940f6d0895b34ccd319e26680e14d5ab8639439
Miguel Augusto Aliaga Pantoja <maliaga.pantoja@gmail.com>
2019-03-17 20:24:28 -0500

    adding auth

--- backup.sql
+++ backup.sql
@@ -10,3 +10,13 @@ create table users(
   dataVerified boolean default false,
   createdAt timestamp default CURRENT_TIMESTAMP
 );
+
+create table credentials(
+  id varchar(20) primary key,
+  user varchar(20) unique,
+  user_email varchar(20) unique,
+  password varchar(20) not null,
+  createdAt timestamp default CURRENT_TIMESTAMP
+);
+
+alter table credentials add constraint fk user_credential foreign key(user) references users(id);
